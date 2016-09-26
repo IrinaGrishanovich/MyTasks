31a3a465de880b23d0d28bd2b801fc9deb868379
Irina Grishanovich <ivgrishanovich@gmail.com>
2016-09-27 00:27:54 +0300

    Add files via upload

---
+++ taskTutBy/src/main/java/base/TestBase.java
@@ -0,0 +1,28 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    private static final String DEFAULT_URL = "http://www.tut.by";
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+        driver.get(DEFAULT_URL);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
--- taskTutBy/src/test/java/InboxLettersTest.java
+++ taskTutBy/src/test/java/InboxLettersTest.java
@@ -1,30 +1,13 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import base.TestBase;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.EmailPage;
 import pages.MainPage;
 
-import java.util.concurrent.TimeUnit;
+public class InboxLettersTest extends TestBase {
 
-public class InboxLettersTest {
-    private static final String DEFAULT_URL = "http://www.tut.by";
     private static final String LOGIN = "Irinatest1";
     private static final String PASSWORD = "12345_irina";
-
-    private WebDriver driver;
-
-    @BeforeMethod
-    public void setup() {
-        driver = new FirefoxDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-        driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
-        driver.get(DEFAULT_URL);
-    }
 
     @Test
     public void checkInboxLetters()  {
@@ -32,11 +15,6 @@ public class InboxLettersTest {
         mainPage.login(LOGIN, PASSWORD);
         EmailPage emailPage = mainPage.goToMail();
         Assert.assertTrue(emailPage.isAmountInboxMailDisplayed(), "The amount Inbox Letters didn't display!");
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
     }
 }
 
